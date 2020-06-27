5c4836b8cc8fbaf065ac22c82b954734fc2be8be
vladimir24032000 <64186717+vladimir24032000@users.noreply.github.com>
2020-06-27 18:28:32 +0300

    0.1v

---
+++ HTML EDITOR/HTMLEditorClass.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using HtmlAgilityPack;
+
+namespace HTML_EDITOR
+{
+    class HTMLEditorClass
+    {
+        MainWindow mainWindow;
+        Label statusBar;
+        string htmlPath;
+        HtmlDocument index;
+
+       public HTMLEditorClass(Label statusBar, MainWindow mainWindow)
+        {
+            this.statusBar = statusBar;
+            this.mainWindow = mainWindow;
+        }
+        public void downlodFile(string url)
+        {
+            WebClient myWebClient = new WebClient();
+            statusBar.Content = "Загрузка..";
+            // Concatenate the domain with the Web resource filename.
+           
+            // Download the Web resource and save it into the current filesystem folder.
+            myWebClient.DownloadFile(url, "index.html");
+            
+
+
+
+        }
+        public void setPath(string htmlPath)
+        {
+            this.htmlPath = htmlPath;
+        }
+        public void openHTML()
+        {
+           
+            index = new HtmlDocument();
+            index.Load(htmlPath);
+            parsing();
+        }
+        public void parsing()
+        {
+            var imgs = index.DocumentNode.SelectNodes("//img");
+            for(int i = 0; i< imgs.Count(); i++)
+            {
+                
+                mainWindow.addNewEditForm(i + 1, 1, imgs[i].GetAttributeValue("src", "NULL"));
+
+            }
+            var links = index.DocumentNode.SelectNodes("//a");
+            for (int i = 0; i < links.Count(); i++)
+            {
+                mainWindow.addNewEditForm(i + 1, 2, links[i].GetAttributeValue("href", "NULL"));
+            }
+            var texts = index.DocumentNode.SelectNodes("//p");
+            for (int i = 0; i < texts.Count(); i++)
+            {
+                mainWindow.addNewEditForm(i + 1, 0, texts[i].InnerText);
+            }
+        }
+        public void changeFile(int tagNumber, int number, string text)
+        {
+            var imgs = index.DocumentNode.SelectNodes("//img");
+            var links = index.DocumentNode.SelectNodes("//a");
+            var texts = index.DocumentNode.SelectNodes("//p");
+            switch (tagNumber)
+            {
+                case 0:
+                    {
+                        
+
+                        texts[number].ParentNode.ReplaceChild(HtmlTextNode.CreateNode("<p>"+ text + "</p>"), texts[number]);
+
+
+                        break;
+                    }
+                case 1:
+                    {
+                        imgs[number].SetAttributeValue("src", text);
+                        break;
+                    }
+                case 2:
+                    {
+                        links[number].SetAttributeValue("href", text);
+                        break;
+                    }
+
+            }
+        }
+        public void saveFile()
+        {
+            index.Save("index.html");
+
+        }
+
+    }
+}
---
+++ HTML EDITOR/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HTML_EDITOR
+{
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HTMLEditorClass hTMLEditorClass;
+        List<TextBox> textsBoxes = new List<TextBox>();
+        List<TextBox> imgsBoxes = new List<TextBox>();
+        List<TextBox> linksBoxes = new List<TextBox>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            hTMLEditorClass = new HTMLEditorClass(statusBar, this);
+        }
+
+        private void downlodButton_Click(object sender, RoutedEventArgs e)
+        {
+            downlodButton.IsEnabled = false;
+            try
+            {
+                hTMLEditorClass.downlodFile(urlTextBox.Text);
+                statusBar.Content = "Файл загружен в " + System.AppDomain.CurrentDomain.BaseDirectory;
+                hTMLEditorClass.setPath(System.AppDomain.CurrentDomain.BaseDirectory + "index.html");
+               if( MessageBox.Show("Открыть файл?", "HTML Editor", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        textsEditStack.Children.Clear();
+                        imgEditStack.Children.Clear();
+                        linksEditStack.Children.Clear();
+                        linksBoxes.Clear();
+                        imgsBoxes.Clear();
+                        textsBoxes.Clear();
+                        hTMLEditorClass.openHTML();
+                    }
+                    catch
+                    {
+                        statusBar.Content = "Ошибка открытия";
+
+                    }
+                }
+                else
+                {
+                    
+                }
+            }
+            catch
+            {
+                statusBar.Content = "Файл не загружен. Попробуйте снова";
+            }
+            downlodButton.IsEnabled = true;
+
+        }
+        public void addNewEditForm(int number, int tagNumber, string curText)
+        {
+            DockPanel dockPanel = new DockPanel();
+            TextBlock numberTextBlock = new TextBlock();
+            numberTextBlock.Text = number.ToString();
+            TextBox textBox = new TextBox();
+            textBox.HorizontalAlignment = HorizontalAlignment.Left;
+            textBox.Width = 300;
+            textBox.Margin = new Thickness(10, 10, 10, 10);
+            textBox.TextChanged += editTextBox_TextChanged;
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = curText;
+            textBlock.TextWrapping = TextWrapping.Wrap;
+            dockPanel.Children.Add(numberTextBlock);
+            dockPanel.Children.Add(textBox);
+            dockPanel.Children.Add(textBlock);
+            switch (tagNumber)
+            {
+                case 0:
+                    {
+                        textsEditStack.Children.Add(dockPanel);
+                        textsBoxes.Add(textBox);
+                        break;
+                    }
+                case 1:
+                    {
+                        imgEditStack.Children.Add(dockPanel);
+                        imgsBoxes.Add(textBox);
+                        break;
+                    }
+                case 2:
+                    {
+                        linksEditStack.Children.Add(dockPanel);
+                        linksBoxes.Add(textBox);
+                        break;
+                    }
+            }
+
+            //< Label Content = "Изменить текст" ></ Label >
+ 
+            //         < DockPanel Margin = "10" >
+  
+            //              < TextBlock Text = "1." ></ TextBlock >
+   
+            //               < TextBox HorizontalAlignment = "Left" Width = "300" Margin = "10,0,10,0" ></ TextBox >
+        
+            //                    < TextBlock Text = "Текущий текст" >
+         
+            //                     </ TextBlock >
+         
+            //                 </ DockPanel >
+        }
+
+       
+        private void editTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            saveButton.IsEnabled = true;
+            TextBox changedBox = (TextBox)sender;
+            for(int i = 0; i < textsBoxes.Count(); i++)
+            {
+                if(changedBox == textsBoxes[i])
+                {
+                    hTMLEditorClass.changeFile(0, i, changedBox.Text);
+                    return;
+                }
+            }
+            for (int i = 0; i < imgsBoxes.Count(); i++)
+            {
+                if (changedBox == imgsBoxes[i])
+                {
+                    hTMLEditorClass.changeFile(1, i, changedBox.Text);
+                    return;
+                }
+            }
+            for (int i = 0; i < linksBoxes.Count(); i++)
+            {
+                if (changedBox == linksBoxes[i])
+                {
+                    hTMLEditorClass.changeFile(2, i, changedBox.Text);
+                    return;
+                }
+            }
+
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            saveButton.IsEnabled = false;
+            hTMLEditorClass.saveFile();
+            statusBar.Content = "Сохранено!";
+        }
+    }
+}
